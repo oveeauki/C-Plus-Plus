bb1b5ce60969e9b8eb1905e0e0da56aaa44afc57
Elias <62937543+oveeauki@users.noreply.github.com>
2020-09-11 16:26:51 +0300

    ⠀

--- Number Sorter.cpp
+++ Number Sorter.cpp
@@ -3,33 +3,24 @@
 using namespace std;
 
 void sort(){
-cout << "Please Enter 6 Different Numbers\n";
 string f;
-long long int ok[6];
-int x = sizeof(ok) / sizeof(ok[0]);
-cout << "Enter Number 1 ";
-cin >> ok[0]; 
-cout << "Enter Number 2 ";
-cin >> ok[1]; 
-cout << "Enter Number 3 ";
-cin >> ok[2];
-cout << "Enter Number 4 ";
-cin >> ok[3]; 
-cout << "Enter Number 5 ";
-cin >> ok[4]; 
-cout << "Enter Number 6 ";
-cin >> ok[5];
-cout << "You Entered Numbers : ";
-for (int i = 0; i < x; i++)
-cout << ok[i] << " ";
+long long int x[7];
+int n = sizeof(x) / sizeof(x[0]);
+cout << "Give 6 Numbers to Sort\n";
+for(int i=1;i<7;i++){
+cout << "Number " << i << " ";
+cin >> x[i];
+}
+cout << "Your Numbers Were ";
+for(int i=1;i<n;i++)
+cout << x[i] << " ";
 cout << "\n";
-sleep(1);
-cout << "Sorting...\n";
-sort(ok,ok+x);
-sleep(2);
-cout << "Your Numbers correct order is : ";
-for (int i = 0; i < x; i++)
-cout << ok[i] << " ";
+cout << "Sorting....\n";
+sleep(3);
+sort(x,x+n);
+cout << "Your Numbers In Correct Order Are\n";
+for(int i=1;i<n;i++)
+cout << x[i] << " ";
 cout << "\n";
 cout << "Sort Again y/N ?\n";
 cin >> f;
@@ -43,14 +34,14 @@ return;
 }
 }
 int main(){
-string u[]={"Number Sorter(1)"," Exit(2)"};
-int j;
-cout << u[0] << u[1] << " ";
-cin >> j;
-if (j == 1){
-sort();
+int y;
+string x[]={"Number Sorter(1)"," Exit(2)"};
+cout << x[0] << x[1] << "\n";
+cin >> y;
+if (y == 1){
+sort();    
 }
-else if (j == 2){
+else if (y == 2){
 cout << "Exit\n";
 sleep(2);
 return 0;    
