bc7a80519cf549ef7d8453a968b6409bb0c49eca
Elias <62937543+oveeauki@users.noreply.github.com>
2021-04-23 12:14:55 +0300

    ⠀

---
+++ ReturnRemainder(Edabit).cpp
@@ -0,0 +1,22 @@
+/*
+            Edabit
+Return the Remainder from Two Numbers
+*/
+#include "iostream"
+using namespace std;
+
+signed modulustaa(short ekanumero,short tokanumero){
+cout << "Remainder of numers: " <<  "["  << ekanumero  <<  "]" << "&" << "[" << tokanumero << "]" << " is = " << (ekanumero%tokanumero) << "\n";  
+return(ekanumero%tokanumero);
+}
+
+signed main(void){
+short lippisint[3];
+for(int i=1;i<3;i++){
+cout << "Number " << i << "\n";
+cout << ": ";
+cin >> lippisint[i];
+}
+modulustaa(lippisint[1],lippisint[2]);
+return 0;
+}
