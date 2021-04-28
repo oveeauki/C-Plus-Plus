0100111bc2426fb3d1dd8c6174dabfde80016657
Elias <62937543+oveeauki@users.noreply.github.com>
2021-04-28 11:10:31 +0300

    ⠀

---
+++ Burrrrrrrp[Edabit].cpp
@@ -0,0 +1,23 @@
+/*
+[Edabit]
+Burrrrrrrp
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+const int burrrrpxd(short maara){
+char burp = 'r';
+string purpp;
+for(short i=0;i<maara;i++){
+purpp.push_back(burp);
+}
+cout << "Bu" << purpp << "p\n"; 
+return purpp[maara];
+}
+
+signed main(void){
+cout << "Enter amount of R's xd: ";
+short ramount;
+cin >> ramount;
+burrrrpxd(ramount);
+}
